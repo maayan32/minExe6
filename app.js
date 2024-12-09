28672a3f8c522f7ae095eae0db9ed3c8a4e510e0
maayan32 <maayanshira.02@gmail.com>
2024-12-09 16:37:49 +0200

    added different requwst url options

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import { createServer } from "http";
 createServer((request, response) => {
+if (request.url === '/foo') {
+response.write('<html><body><b>foo</b></body></html>');
+} else if (request.url === '/bar') {
+response.write('<html><body><u>bar</u></body></html>');
+} else {
 response.statusCode = 404;
-response.end("Hello world");
-}).listen(80);
+}
+response.end();
+}).listen(8080);
