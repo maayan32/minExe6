7ebadddaedbebcccf573a1de4d3c3de9c4ce9a44
maayan32 <maayanshira.02@gmail.com>
2024-12-10 11:28:35 +0200

    turned app (in calc) to get username and password using get

--- calc/app.js
+++ calc/app.js
@@ -3,30 +3,16 @@ const app = express()
 app.use(express.static('public'))
 const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({extended : true}))
-function calc(x,y,operation){
-switch (operation) {
-case '0':
-return x+y
-case '1':
-return x-y
-case '2':
-return x*y
-case '3':
-return x/y
-default:
-return 'incalculable'
+
+app.get('/login', function(req,res){
+var user = req.query.username;
+var password = req.query.password;
+if((user == 'guest') && (password == 'pass')){
+    res.end('welcome')
 }
+else{
+    res.end('incorrect username and/or password')
 }
-app.post('/calc', function(req,res){
-var x = parseInt(req.body.x)
-var y = parseInt(req.body.y)
-var operation = req.body.operation
-var result = calc(x,y,operation)
-res.end(`<html>
-<body>
-The answer is ${result}.
-<a href="/index.html">reset</a>
-</body>
-</html>`)
+
 })
-app.listen(880)
+app.listen(8080)
