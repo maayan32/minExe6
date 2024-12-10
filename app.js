36543cdc070a816241c767da37dff8e7443ccc68
maayan32 <maayanshira.02@gmail.com>
2024-12-10 10:35:43 +0200

    added image  and html to public

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 import express from 'express';
 const app = express();
+app.use(express.static('public'));
 app.get('/', (req, res) => {
 res.send('<html><body><h1>GET</h1></body></html>');
-});
-app.post('/', (req, res) => {
-res.send('<html><body><h1>POST</h1></body></html>');
 });
 app.listen(8080);
